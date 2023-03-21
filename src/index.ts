a655d2f902f030bde50460f87d4de3c9065bed9c
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2023-03-21 01:39:36 -0300

    feat: list lambda functions' environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('lets get starteddd')
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { ListFunctionsCommand } from '@aws-sdk/client-lambda'
+import { lambdaClient } from './AWSClients/lambdaClient'
+import { log } from 'ads-mylog'
+;(async () => {
+	try {
+		log.debug('trying get lambda functions')
+		const cmd = new ListFunctionsCommand({})
+		const res = await lambdaClient.send(cmd)
+		res.Functions?.map((lf) =>
+			log.debug(JSON.stringify(lf.Environment?.Variables, null, 4))
+		)
+	} catch (err) {
+		log.error(err)
+	}
+})()
